4073e3aa98cfd154dfd98a4a76f0a0803a541e2a
Kacper Gunia <kacper@gunia.me>
2016-03-31 23:13:23 +0100

    Add Neo4J

--- apps/recommendation/web/index.php
+++ apps/recommendation/web/index.php
@@ -1,11 +1,25 @@
 <?php
 
 
+use Neoxygen\NeoClient\ClientBuilder;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->get('/{readerId}/recommendations', function ($readerId) use($app) {
+$app['neo4j'] = function () {
+    return ClientBuilder::create()
+        ->addDefaultLocalConnection()
+        ->build();
+};
+
+$app->get('/{readerId}/recommendations', function ($readerId) use ($app) {
+    return $app->json([]);
+});
+
+
+$app->get('/{readerId}/read/{bookId}', function ($readerId, $bookId) use ($app) {
+
     return $app->json([]);
 });
 
